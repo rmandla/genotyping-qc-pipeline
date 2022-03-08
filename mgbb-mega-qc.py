51c122cf99a6d8025cd1d4ec90781662e45cf510
Ravi Mandla <rmandla@wm906-f71.broadinstitute.org>
2022-03-07 20:02:20 -0500

    create script for analysis

---
+++ mgbb-mega-qc.py
@@ -0,0 +1,15 @@
+import os, subprocess
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from general_qc import *
+
+plink2 = '/humgen/diabetes/users/josep/apps/plink2/plink' # plink2 executable
+directory = '/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/' # directory containing starting plink bed/bim/fam files
+header = 'PHBB_SNPs_35K' # header of starting plink bed/bim/fam files
+ancestry_file_path = directory + 'biobank_oct2019_with_subject_ID.csv' # tab-delimited file containing
+ancestry_matrix = {'EU':['W'],'HIS':['H'],'AFR':['B'],'AS':['A'],'Other':['O','']}
+pheno_files = ['file1.bin.txt','file2.bin.txt','file3.bin.txt','file4.bin.txt','file5.bin.txt','file6.bin.txt','file7.bin.txt','file8.bin.txt']
+
+run_qc(plink2,directory,header,ancestry_file_path,ancestry_matrix,pheno_files)
